eeab057c1f1ee76dc10bec20377fa93ab2700270
Aykhan <aykhan.karaman@gmail.com>
2022-01-30 19:24:55 -0800

    add first tests add mocha config

---
+++ specs/example.spec.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+
+describe( title: 'Math function', fn: function() {
+
+}() {
+    it(title: 'A +B = C', fn: function() {
+        const a = 4
+        const b = 5
+        const c = 9
+        const d = -1
+
+       it(titlr 'A + B =C', fn: function() {
+           expect( val: a + b) .to.eq(c)
+  })
+it(title: 'A- B =D', function() {
+    expect(val: a - b).to.eq(d)
+ })
+})
